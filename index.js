66ec01f7b6113436e202ce78ebf63e9fd0472fa6
Niklas Pelli <niklaspelli22@gmail.com>
2023-12-04 14:00:37 +0100

    fixat users

--- index.js
+++ index.js
@@ -1,128 +1,65 @@
-let express = require("express"); 
-let app = express();
-app.listen(8080);
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 console.log("Niklas API körs på port 8080");
 
-app.use(express.urlencoded({ extended: true })); 
+app.listen(8080);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/main.html");
 });
 
 const mysql = require("mysql");
-  databas = mysql.createConnection({
-  host: "localhost", 
-  user: "root", 
-  password: "", 
-  database: "niklasforum", 
-  multipleStatements: true, 
+const databas = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "niklasforum",
+  multipleStatements: true,
 });
 
-const crypto = require("crypto"); 
+app.use(express.json());
+
 function hash(data) {
   const hash = crypto.createHash("sha256");
   hash.update(data);
   return hash.digest("hex");
 }
-  
-const tokenverktyg = require("jsonwebtoken"); 
-app.post("/login", function (req, res) {
-  console.log(req.body);
-  if (!(req.body && req.body.username && req.body.password)) {
-    res.sendStatus(400);
-    return;
-  }
-  app.get("/login", function (req, res) {
-  let sqlbas = `SELECT * FROM users WHERE username='${req.body.username}'`;
 
-  databas.query(sqlbas, function (err, result, fields) {
-    if (err) throw err;
-    if (result.length == 0) {
-      res.sendStatus(401);
-      return;
-    }
-    let passwordHash = hash(req.body.password);
-    console.log(passwordHash);
-    console.log(result[0].password);
-    if (result[0].password == passwordHash) {
-      res.send({
-        
-        name: result[0].name,
-        username: result[0].username,
-       
-      });
-    } else {
-      res.sendStatus(401);
-    }
-  });
-});
-  
-app.get("/users", function (req, res) {
-
-  let authHeader = req.headers["authorization"];
-  if (authHeader === undefined) {
-    res.sendStatus(400); 
-    return;
-  }
-  let token = authHeader.slice(7); 
-  
-  console.log(token);
-
-  let decoded;
-  try {
-    decoded = jwt.verify(token, "EkkorenSattiTaket12=%%");
-  } catch (err) {
-    console.log(err); 
-    res.status(401).send("Invalid auth token");
-    return;
-  }
-
-  console.log(decoded);
-  console.log(`Halloj! ${decoded.name}! Din mejladress är ${decoded.email}.`);
-
-  app.get("/users", function (req, res) {
-  let sqlbas = "SELECT * FROM niklasforum"; 
-  console.log(sqlbas);
- 
-  databas.query(sqlbas, function (err, result, fields) {
-    res.send(result);
-  });
-  });
-});
-
-const ANVANDARE = ["id", "username", "password", "name"]; 
+const ANVANDARE = ["id", "username", "password", "name"];
 
 app.get("/users", function (req, res) {
-  let sqlbas = "SELECT * FROM niklasforum";
- let condition = createCondition(req.query);
-  console.log(sqlbas + condition);
-  databas.query(sqlbas + condition, function (err, result, fields) {
+  let sql = "SELECT * FROM niklasforum";
+  let condition = createCondition(req.query);
+  console.log(sql + condition);
+
+  databas.query(sql + condition, function (err, result, fields) {
     res.send(result);
   });
-}); 
-
+});
 
 let createCondition = function (query) {
   console.log(query);
   let output = " WHERE ";
   for (let key in query) {
     if (ANVANDARE.includes(key)) {
-     
-      output += `${key}="${query[key]}" OR `; 
+      output += `${key}="${query[key]}" OR `;
     }
   }
   if (output.length == 7) {
-    return ""; 
+    return "";
   } else {
-    return output.substring(0, output.length - 4); 
+    return output.substring(0, output.length - 4);
   }
 };
 
-});
 app.get("/users/:id", function (req, res) {
-  let sqlbas = "SELECT * FROM niklasforum WHERE id=" + req.params.id;
-  console.log(sqlbas);
-  databas.query(sqlbas, function (err, result, fields) {
+  let sql = "SELECT * FROM niklasforum WHERE id=" + req.params.id;
+  console.log(sql);
+
+  databas.query(sql, function (err, result, fields) {
     if (result.length > 0) {
       res.send(result);
     } else {
@@ -131,8 +68,61 @@ app.get("/users/:id", function (req, res) {
   });
 });
 
-app.use(express.json());
+app.post("/login", function (req, res) {
+  console.log(req.body);
+  if (!(req.body && req.body.username && req.body.password)) {
+    res.sendStatus(400);
+    return;
+  }
+  let sql = `SELECT * FROM niklasforum WHERE username='${req.body.username}'`;
 
+  databas.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    let passwordHash = hash(req.body.password);
+    if (result[0].password == passwordHash) {
+      let payload = {
+        sub: result[0].username,
+        name: result[0].name,
+      };
+      let token = jwt.sign(payload, "EnHemlighetSomIngenKanGissaXyz123%&/");
+      res.json(token);
+    } else {
+      res.sendStatus(401);
+    }
+  });
+});
+
+app.get("/users", function (req, res) {
+  let authHeader = req.headers["authorization"];
+  if (authHeader === undefined) {
+    res.sendStatus("400");
+    return;
+  }
+  let token = authHeader.slice(7);
+
+  console.log(token);
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, "EnHemlighetSomIngenKanGissaXyz123%&/");
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Invalid auth token");
+    return;
+  }
+
+  console.log(decoded);
+  console.log(
+    `Hallojsan! ${decoded.name}! Din mailadress är ${decoded.email}.`
+  );
+
+  let sql = "SELECT * FROM niklasforum";
+  console.log(sql);
+
+  databas.query(sql, function (err, result, fields) {
+    res.send(result);
+  });
+});
 
 app.post("/users", function (req, res) {
   if (!req.body.username) {
@@ -143,28 +133,28 @@ app.post("/users", function (req, res) {
   for (let key in req.body) {
     if (!fields.includes(key)) {
       res.status(400).send("Unknown field: " + key);
-      return; 
+      return;
     }
   }
 
-  let sqlbas = `INSERT INTO niklasforum (username, name, password)
-    VALUES ('${req.body.username}', 
-    '${req.body.name}',
-    '${hash(req.body.password)}');
-    SELECT LAST_INSERT_ID();`; 
-  console.log(sqlbas);
-  
-databas.query(sqlbas, function (err, result, fields) {
+  let sql = `INSERT INTO niklasforum (username, password, name)
+      VALUES ('${req.body.username}', 
+      '${hash(req.body.password)}',
+      '${req.body.name}');
+      SELECT LAST_INSERT_ID();`;
+  console.log(sql);
+
+  databas.query(sql, function (err, result, fields) {
     if (err) throw err;
+
     console.log(result);
     let output = {
       id: result[0].insertId,
+      username: req.body.username,
       name: req.body.name,
-      username: req.body.username,    
-    }; 
-  
-    res.send(output);  
-  }); 
+    };
+    res.send(output);
+  });
 });
 
 app.put("/users/:id", function (req, res) {
@@ -176,16 +166,27 @@ app.put("/users/:id", function (req, res) {
         SET name = '${req.body.name}',password = '${hash(req.body.password)}'
         WHERE id = ${req.params.id}`;
 
-        databas.query(sqlbas, function (err, result, fields) {
+  databas.query(sqlbas, function (err, result, fields) {
     if (err) {
+      res.status(400).send("Något gick fel!");
       throw err;
     } else {
-      res.sendStatus(200);
+      res.status(200).send("Allt gick galant!");
     }
-    });
   });
+});
 
-   app.delete("/users/:id", (req,res) => {
+/* app.delete("/users/:id", (req, res) => {
+  const deleteUser = deleteUsers.find(c => c.id === parseInt(req.params.id));
+  if (!deleteUser) res.status(404).send('Användaren hittades ej!');
+  const index = deleteUsers.indexOf(deleteUser);
+  deleteUsers.splice(index, 1);
+
+  res.send(deleteUser);
+})
+ */
+
+/*   app.delete("/users/:id", (req,res) => {
     try{
       const {id} = req.params;
       const deleteUser =  User.findByIdAndDelete(id);
@@ -196,4 +197,4 @@ app.put("/users/:id", function (req, res) {
     } catch (error) {
       res.status(500).json({message: error.message})
     }
-  }) 
+  })  */
